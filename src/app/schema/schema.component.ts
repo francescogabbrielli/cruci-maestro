5089757a16f4c1708bdd0e1065a1ce7678443d12
Francesco Gabbrielli <francescogabbrielli@gmail.com>
2020-04-05 09:12:29 +0200

    Cruci navigation with arrows and word highlight

--- src/app/schema/schema.component.ts
+++ src/app/schema/schema.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { OnInit } from '@angular/core';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import $ from "jquery";
@@ -16,11 +16,19 @@ export class SchemaComponent implements OnInit {
   lastReframe:number                    = 0;
   dragPosition:{x:number, y:number};
   dragging:boolean                      = false;
+  state:{
+    x:number,
+    y:number
+    horizontal:boolean,
+    start:number,
+    end:number
+  } = {x:0, y:0, horizontal: true, start: 0, end: this.size.cols-1};
 
   ngOnInit() {
     this.reframe(this.size);
     $(document).mousemove(event=>this.resize(event));
     $(document).mouseup(event=>this.prepareDrag(false, event));
+    this.highlightCurrentWord();
   }
 
   reframe(s) {
@@ -44,6 +52,7 @@ export class SchemaComponent implements OnInit {
     //stop dragging
     if(this.dragging && !activate) {
       this.size = this.resizing;
+      console.log("RESIZE", this.size);
     }
     this.dragging = activate;
     this.dragPosition = {x: event.clientX, y: event.clientY};
@@ -60,9 +69,89 @@ export class SchemaComponent implements OnInit {
       cols: this.size.cols + Math.trunc(diffX/this.cellSize)
     };
     let t = new Date().getTime();
-    if (newSize!=this.resizing && t > this.lastReframe+50) {
+    if (newSize!==this.resizing && t > this.lastReframe+50) {
       this.lastReframe = t;
       this.reframe(newSize);
+    }
+  }
+
+  highlightCurrentWord() {
+    let start, end;
+    if (this.state.horizontal) {
+      start = this.state.x;
+      for (let i=this.state.x; i>=0; i--)
+        if (this.cells[this.state.y][i]==='.')
+          break;
+        else start = i;
+      for (let i=this.state.x; i<this.size.cols; i++)
+        if (this.cells[this.state.y][i]==='.')
+          break;
+        else end = i;
+    } else {
+      for (let i=this.state.y; i>=0; i--)
+        if (this.cells[i][this.state.x]==='.')
+          break;
+        else start = i;
+      for (let i=this.state.y; i<this.size.rows; i++)
+        if (this.cells[i][this.state.x]==='.')
+          break;
+        else end = i;
+    }
+    this.state.start = start;
+    this.state.end = end;
+  }
+
+  moveCursor(x:number, y:number) {
+    let highlight = x!==this.state.x && y!==this.state.y;
+    if (!highlight && this.cells[this.state.y][this.state.x] === ".")
+      highlight = true;
+    if (x>=0 && x<this.size.cols && y>=0 && y<this.size.rows) {
+      this.state.x = x;
+      this.state.y = y;
+    }
+    if (!highlight && this.cells[this.state.y][this.state.x] === ".")
+      highlight = true;
+    if (highlight)
+      this.highlightCurrentWord();
+  }
+
+  toggleOrientation(horizontal?:boolean) {
+    if (horizontal == undefined || horizontal!==this.state.horizontal) {
+      this.state.horizontal = !this.state.horizontal;
+      this.highlightCurrentWord();
+    }
+  }
+
+  @HostListener('window:keydown', ['$event'])
+  keyDown(event: KeyboardEvent) {
+    if (event.key === "ArrowRight") {
+      this.moveCursor(this.state.x+1, this.state.y);
+      this.toggleOrientation(true);
+    }
+    else if (event.key === "ArrowLeft") {
+      this.moveCursor(this.state.x-1, this.state.y);
+      this.toggleOrientation(true);
+    }
+    else if (event.key === "ArrowDown") {
+      this.moveCursor(this.state.x, this.state.y+1);
+      this.toggleOrientation(false);
+    }
+    else if (event.key === "ArrowUp") {
+      this.moveCursor(this.state.x, this.state.y-1);
+      this.toggleOrientation(false);
+    }
+    else if (event.key === "Enter") {
+      this.toggleOrientation();
+    }
+  }
+
+  @HostListener('window:keyup', ['$event'])
+  keyUp(event: KeyboardEvent) {
+    if (event.key===".") {
+      this.cells[this.state.y][this.state.x] = event.key;
+      this.highlightCurrentWord();
+    } else if (event.key.match(/^[a-zA-Z]$/)) {
+      this.cells[this.state.y][this.state.x] = event.key.toUpperCase();
     }
   }
 
